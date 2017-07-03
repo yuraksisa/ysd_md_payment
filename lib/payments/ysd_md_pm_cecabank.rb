c27a7d938243443c3092fe5669a2d34a2564c645
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2017-07-03 15:24:24 +0200

    bug in notification from cecabank

--- lib/payments/ysd_md_pm_cecabank.rb
+++ lib/payments/ysd_md_pm_cecabank.rb
@@ -26,14 +26,14 @@ module Payments
         <form action="<%=ceca_url%>" method="POST" 
               enctype="application/x-www-form-urlencoded"
               name="gateway">
-          <input name="MerchantId" type="hidden" value="<%=merchant_id%>"/>
-          <input name="AcquirerId" type="hidden" value="<%=acquirer_id%>"/>
-          <input name="TerminalId" type="hidden" value="<%=terminal_id%>"/>
+          <input name="MerchantID" type="hidden" value="<%=merchant_id%>"/>
+          <input name="AcquirerBIN" type="hidden" value="<%=acquirer_id%>"/>
+          <input name="TerminalID" type="hidden" value="<%=terminal_id%>"/>
           <input name="URL_OK" type="hidden" value="<%=url_ok%>"/>
           <input name="URL_NOK" type="hidden" value="<%=url_nok%>"/>
           <input name="Firma" type="hidden" 
-                 value="<%=firma(num_operacion, importe)%>"/>
-          <input name="Cifrado" type="hidden" value="SHA1"/>        
+                 value="<%=firma(num_operacion, format_amount(importe))%>"/>
+          <input name="Cifrado" type="hidden" value="SHA2"/>        
           <input name="Num_operacion" type="hidden" value="<%=num_operacion%>">
           <input name="Importe" type="hidden" value="<%=format_amount(importe)%>">
           <input name="TipoMoneda" type="hidden" value="978"/>
@@ -49,12 +49,72 @@ module Payments
       EOF
 
       template = Tilt.new('erb'){result}
-      template.render(self, {:num_operacion => charge.id, 
+      template.render(self, {:num_operacion => charge.id.to_s,
       	                     :importe => charge.amount})      
 
     end
 
+    #
+    # Calculate the signature notification from its parameters
+    #
+    def notification_signature(ns_merchant_id, 
+                               ns_acquirer_bin,
+                               ns_terminal_id, 
+                               ns_num_operacion, 
+                               ns_importe, 
+                               ns_tipo_moneda,
+                               ns_exponente, 
+                               ns_referencia)
+
+      signature = ""
+      signature << clave_encriptacion
+      signature << ns_merchant_id
+      signature << ns_acquirer_bin
+      signature << ns_terminal_id
+      signature << ns_num_operacion
+      signature << ns_importe
+      signature << ns_tipo_moneda
+      signature << ns_exponente
+      signature << 'SHA2'
+      signature << url_ok
+      signature << url_nok
+
+      Digest::SHA256.hexdigest signature
+
+    end
+
     private
+
+    #
+    # Firma
+    #
+    # @return [String]
+    #
+    def firma(num_operacion, importe)
+
+      signature = ""
+      signature << clave_encriptacion
+      signature << merchant_id
+      signature << acquirer_id
+      signature << terminal_id
+      signature << num_operacion
+      signature << importe
+      signature << '978'
+      signature << '2'
+      signature << 'SHA2'
+      signature << url_ok
+      signature << url_nok
+
+      Digest::SHA256.hexdigest signature
+
+    end
+
+    #
+    # Format the amount to send it
+    #
+    def format_amount(amount)
+      ("%.2f" % amount).gsub('.','')
+    end
 
     def ceca_url
       SystemConfiguration::SecureVariable.get_value('payments.cecabank.url')
@@ -82,31 +142,6 @@ module Payments
 
     def url_nok
       SystemConfiguration::SecureVariable.get_value('payments.cecabank.url_nok')
-    end
-
-    #
-    # Firma
-    #
-    # @return [String] 
-    #
-    def firma(num_operacion, importe)
-      texto = ""
-      texto << clave_encriptacion
-      texto << merchant_id
-      texto << acquirer_id
-      texto << terminal_id
-      texto << num_operacion
-      texto << format_amount(importe)
-      texto << '978'
-      texto << '2'
-      texto << 'SHA1'
-      texto << url_ok
-      texto << url_nok
-      Digest::SHA1.hexdigest texto
-    end
-
-    def format_amount(amount)
-      ("%.2f" % amount).gsub('.','')
     end
 
   end
